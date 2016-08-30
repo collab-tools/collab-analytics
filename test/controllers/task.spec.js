87416ba00683525c39305b1e947d0abd7f352a59
Yeap Hooi Tong <hooitongx@gmail.com>
2016-08-30 15:31:33 +0800

    Remove dependency of test runners in test file

--- test/controllers/task.spec.js
+++ test/controllers/task.spec.js
@@ -1,9 +1,3 @@
-const chai = require('chai');
-const sinon = require('sinon');
-const sinonChai = require('sinon-chai');
-chai.should();
-chai.use(sinonChai);
-
 describe('Logging Task Activities', () => {
   it('should save the given milestone activity into the database', () => {
 
